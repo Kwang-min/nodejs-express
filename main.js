18f5e68891f4126fba0283010d4530e5d46e3fd0
Kwang-min <48762168+Kwang-min@users.noreply.github.com>
2020-07-23 23:02:14 +0900

    middleware-bodyParser
    -

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (request, response) {
   fs.readdir('./data', function (error, filelist) {
@@ -64,19 +67,15 @@ app.get('/create', function (request, response) {
 });
 
 app.post('/create_process', function (request, response) {
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.writeHead(302, { Location: `/?id=${title}` });
-      response.end();
-    })
-  });
+
+  request.body
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+    response.writeHead(302, { Location: `/?id=${title}` });
+    response.end();
+  })
 });
 
 app.get('/update/:pageId', function (request, response) {
@@ -105,37 +104,26 @@ app.get('/update/:pageId', function (request, response) {
   });
 });
 
-app.post('/update_process', function(request,response){
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.redirect(`/?id=${title}`);
-      })
-    });
+app.post('/update_process', function (request, response) {
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      response.redirect(`/?id=${title}`);
+    })
   });
 });
 
 app.post('/delete_process', function(request,response){
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
+
+    var post = request.body;
     var id = post.id;
     var filteredId = path.parse(id).base;
     fs.unlink(`data/${filteredId}`, function (error) {
       response.redirect('/');
     })
-  });
 });
 
 
